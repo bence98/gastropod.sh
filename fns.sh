5993f4adacd30e52287475950888c8025c3dbe7c
Bence Cs <bence.csokas@gmail.com>
2023-04-12 00:45:00 +0200

    Add aliases.sh and fns.sh

---
+++ fns.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+function mkwd(){
+	mkdir -p "$@" && cd "$_"
+}
+
+function findeditor(){
+	[ "x$EDITOR" != "x" ] && echo "$EDITOR" && return
+
+	[ "x$EDITORPATH" = "x" ] && EDITORPATH="/usr/bin/mcedit /bin/nano /usr/bin/nano /usr/bin/vim /usr/bin/vi /usr/bin/vim.tiny"
+
+	for editor in $EDITORPATH
+	do
+	[ -x "$editor" ] && echo "$editor" && return
+	done
+}
+
+function ednote(){
+	[ "x$1" == "x" ] && echo "Missing parameter!" >&2 && return 1
+	$(findeditor) "$1"_$(date +"%y-%m-%d").md
+}
