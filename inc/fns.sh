33c239e82261c70c0c3cc8a103ae3f06a0ce017f
Csókás, Bence <csokas.bence@prolan.hu>
2024-05-16 16:04:37 +0200

    Add title() for setting terminal title

--- inc/fns.sh
+++ inc/fns.sh
@@ -35,3 +35,12 @@ function upg(){
 		return 1
 	fi
 }
+
+# Source: https://askubuntu.com/a/1164880
+function title() {
+	# Set terminal tab title. Usage: title "new tab name"
+	prefix=${PS1%%\\a*}                  # Everything before: \a
+	search=${prefix##*;}                 # Eeverything after: ;
+	esearch="${search//\\/\\\\}"         # Change \ to \\ in old title
+	PS1="${PS1/$esearch/$@}"             # Search and replace old with new
+}
