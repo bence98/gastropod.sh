3d7a855dde1f641b640ad935d16badd9b7864f9e
Csókás, Bence <csokas.bence@prolan.hu>
2024-05-17 14:43:03 +0200

    Add Device Tree differ

---
+++ inc/20-dt.sh
@@ -0,0 +1,25 @@
+dtdiff() {
+	SOURCE="$1"
+	DEST="$2"
+
+	WORKDIR=$(mktemp -d)
+	trap -- "rm -rf $WORKDIR; trap -- - RETURN" RETURN
+	mkdir -p -- $WORKDIR/{src,dst}
+
+	if [ -d "$SOURCE" ]
+	then
+		for dtb in "$SOURCE"/*.dtb
+		do
+			dts="$(basename ${dtb/dtb/dts})"
+			dtc -sq "$dtb" >"$WORKDIR/src/$dts"
+			dtc -sq "$DEST/$(basename "$dtb")" >"$WORKDIR/dst/$dts"
+		done
+		isdts=
+	else
+		dtc -sq "$SOURCE" >"$WORKDIR/src.dts"
+		dtc -sq "$DEST" >"$WORKDIR/dst.dts"
+		isdts=.dts
+	fi
+
+	gdiff $WORKDIR/{src,dst}$isdts
+}
