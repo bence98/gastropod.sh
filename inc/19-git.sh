e59ff1705f2a56212201782f13f585c28d88e4d7
Csókás, Bence <csokas.bence@prolan.hu>
2024-06-05 17:22:28 +0200

    git: Add `gitmail`, `gitfm` for Linux Git flow

--- inc/19-git.sh
+++ inc/19-git.sh
@@ -2,3 +2,14 @@ alias commitln="git log --oneline -1 --decorate=no"
 alias gdiff="git diff -w --no-index"
 alias chdiff="gdiff --color-words=."
 alias gitaa="git commit -a --amend"
+
+function gitmail(){
+	mkdir -p sent
+	# Thanks https://www.marcusfolkesson.se/blog/get_maintainers-and-git-send-email/
+	git send-email --to-cmd="./scripts/get_maintainer.pl --nogit --nogit-fallback --norolestats --nom --nor" --cc-cmd="./scripts/get_maintainer.pl --nogit --nogit-fallback --norolestats --nol" out/* &&
+	ask_N "Move out to sent" && mv -vi out/* sent
+}
+
+function gitfm(){
+	git format-patch --output-directory=out $@ && ./scripts/checkpatch.pl out/* && gitmail
+}
