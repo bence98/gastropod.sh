7a87517e0450c84a407b6b3ba37acfb31db61b43
Csókás, Bence <csokas.bence@prolan.hu>
2024-06-19 15:00:00 +0200

    19-git.sh: Add optional file arguments to `gitmail`

--- inc/19-git.sh
+++ inc/19-git.sh
@@ -4,10 +4,11 @@ alias chdiff="gdiff --color-words=."
 alias gitaa="git commit -a --amend"
 
 function gitmail(){
+	MBOXES=${@:-out/*}
 	mkdir -p sent
 	# Thanks https://www.marcusfolkesson.se/blog/get_maintainers-and-git-send-email/
-	git send-email --to-cmd="./scripts/get_maintainer.pl --nogit --nogit-fallback --norolestats --nom --nor" --cc-cmd="./scripts/get_maintainer.pl --nogit --nogit-fallback --norolestats --nol" out/* &&
-	ask_N "Move out to sent" && mv -vi out/* sent
+	git send-email --to-cmd="./scripts/get_maintainer.pl --nogit --nogit-fallback --norolestats --nom --nor" --cc-cmd="./scripts/get_maintainer.pl --nogit --nogit-fallback --norolestats --nol" ${MBOXES} &&
+	ask_N "Move out to sent" && mv -vi ${MBOXES} sent
 }
 
 function gitfm(){
