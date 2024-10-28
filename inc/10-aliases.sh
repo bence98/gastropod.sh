2258e7632c40d43bd64e3733eaef8c5510e2c402
Csókás, Bence <csokas.bence@prolan.hu>
2024-10-28 08:00:08 +0100

    aliases: Add `tZ`: multi-threaded Zstd tar

--- inc/10-aliases.sh
+++ inc/10-aliases.sh
@@ -3,3 +3,5 @@ alias ipa="ip -br -c a"
 alias ipl="ip -br -c l"
 alias mcfg="make menuconfig"
 alias mj="make -j$(nproc)"
+# Source: https://unix.stackexchange.com/a/608208
+alias tZ="tar -I 'zstd -22 --ultra --long -T0'"
